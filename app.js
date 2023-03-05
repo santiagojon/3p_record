1131e484a948706705ab8ab1d5fad99de1ac1430
Jon Santiago <jonwsantiago@gmail.com>
2023-03-05 16:10:58 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,40 @@
+async function draw() {
+  // Data
+  const dataset = await d3.csv("data.csv");
+
+  const xAccessor = (d) => d.date;
+  const yAccessor = (d) => d.close;
+
+  // Dimensions
+  let dimensions = {
+    width: 1000,
+    height: 500,
+    margins: 50,
+  };
+
+  dimensions.ctrWidth = dimensions.width - dimensions.margins * 2;
+  dimensions.ctrHeight = dimensions.height - dimensions.margins * 2;
+
+  // Draw Image
+  const svg = d3
+    .select("#chart")
+    .append("svg")
+    .attr("width", dimensions.width)
+    .attr("height", dimensions.height);
+
+  const ctr = svg
+    .append("g") // <g>
+    .attr(
+      "transform",
+      `translate(${dimensions.margins}, ${dimensions.margins})`
+    );
+
+  // Scales
+  const yScale = d3
+    .scaleLinear()
+    .domain(d3.extent(dataset, yAccessor))
+    .range([dimensions.ctrHeight, 0])
+    .nice();
+}
+
+draw();
